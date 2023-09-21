e4a087851044c11f8bf0bcac906d140d82298915
ywonchae1 <ywonchae62@gmail.com>
2023-09-22 01:51:08 +0900

    #1 feat: 합격자 정보 추가 성공 navigate 경로 추가

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AddUserScreen from './src/screens/AddUserScreen';
+import AddUserSuccessScreen from './src/screens/AddUserSuccessScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="AddUserScreen" component={AddUserScreen} />
-        <Stack.Screen name="AddUserSuccess" component={AddUserScreen} />
+        <Stack.Screen name="AddUserSuccess" component={AddUserSuccessScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
