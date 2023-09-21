6e58e5268522bbd24484eabd63a5f431ec8a7cc2
ywonchae1 <ywonchae62@gmail.com>
2023-09-22 01:55:24 +0900

    #1 feat: 합격자 등록 - 성공 페이지

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,20 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { FontStyledText } from './Text'
+import { COLORS } from '../assets/Theme'
 
-export const MainButton = () => {
+export const MainButton = ({children}) => {
   return (
     <View style={styles.container}>
-      <FontStyledText style={styles.text}>다음</FontStyledText>
+      <FontStyledText style={styles.text}>{children}</FontStyledText>
+    </View>
+  )
+}
+
+export const MiniButton = ({outline, children}) => {
+  return (
+    <View style={miniStyles({outline}).miniContainer}>
+      <FontStyledText style={miniStyles({outline}).miniText}>{children}</FontStyledText>
     </View>
   )
 }
@@ -15,7 +24,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
     height: 70,
-    backgroundColor: '#0BEC12',
+    backgroundColor: COLORS.green,
     borderRadius: 15,
   },
   text: {
@@ -23,4 +32,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#000000',
   },
+})
+
+const miniStyles = ({outline}) => StyleSheet.create({
+  miniContainer: {
+    justifyContent: 'center',
+    width: '100%',
+    height: 50,
+    backgroundColor: outline ? 'transparent' : COLORS.green,
+    borderRadius: 13,
+    borderColor: outline ? COLORS.green : 'none',
+    borderWidth: 3,
+  },
+  miniText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: outline ? 'white' : '#000000',
+  }
 })
