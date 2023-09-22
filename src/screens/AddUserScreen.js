235fd50babba845f55f3ddc9cb9a9a4aa5479be7
ywonchae1 <ywonchae62@gmail.com>
2023-09-22 18:11:02 +0900

    #1 feat: 입력 데이터 state 관리, 유효성 점검, fetch data 함수 추가

--- src/screens/AddUserScreen.js
+++ src/screens/AddUserScreen.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Keyboard } from 'react-native'
 import React, { useEffect } from 'react'
 import { COLORS } from '../assets/Theme'
 import { MainButton } from '../components/Button'
@@ -10,25 +10,50 @@ export default function AddUserScreen({ navigation }) {
   const {
     titleNum,
     setTitleNum,
-    onPressNext
+    onPressNext,
+    level, name, phone, email,
+    onInputLevel,
+    onInputName,
+    onInputPhone,
+    onInputEmail,
+    saveUserInfo
   } = useAdduser();
   
-  useEffect(() => {
-    if(titleNum > 4) {
+  const saveUserInfoAndNavigate = async () => {
+    try {
+      await saveUserInfo(level, name, phone, email);
       navigation.navigate('AddUserSuccess');
       setTitleNum(0);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if(titleNum === 4) {
+      Keyboard.dismiss();
+    }
+    if(titleNum > 4) {
+      saveUserInfoAndNavigate();
     }
   }, [titleNum]);
 
-  const onPress = () => {
+  const onPressBack = () => {
     if(titleNum > 0) setTitleNum(titleNum - 1);
+    else ; // 뒤로가기 구현 필요
   }
 
   return (
     <View style={styles.container}>
-      <AddUserHeader onPress={onPress} />
+      <AddUserHeader onPressBack={onPressBack} />
       <AddUserContent
         titleNum={titleNum}
+        level={level} name={name}
+        phone={phone} email={email}
+        onInputLevel={onInputLevel}
+        onInputName={onInputName}
+        onInputPhone={onInputPhone}
+        onInputEmail={onInputEmail}
       />
       <TouchableOpacity onPress={onPressNext}>
         <MainButton>다음</MainButton>
